391ce9fceb430f73ca4cacd8d602df87059ff7a4
MaxPerl <max.augsburg@gmx.de>
2019-01-11 19:37:32 +0100

    Add files via upload

--- lib/MPV/Simple/Pipe.pm
+++ lib/MPV/Simple/Pipe.pm
@@ -27,34 +27,42 @@ our @EXPORT = qw(
 );
 
 use IO::Handle;
+use IO::Select;
+use Symbol qw(qualify_to_ref);
 use MPV::Simple;
 use strict;
 use warnings;
 
-sub new {
-    my ($reader, $writer);
+# Avoid zombies
+$SIG{CHLD} = 'IGNORE';
     
-    # Avoid zombies
-    $SIG{CHLD} = 'IGNORE';
+
+sub new {
+    my ($reader, $writer,$reader2, $writer2);
     
     
     # Fork
     pipe $reader, $writer;
+    pipe $reader2, $writer2;
     $writer->autoflush(1);
+    $writer2->autoflush(1);
     my $pid = fork();
     
     if ($pid != 0) {
         close $reader;
+        close $writer2;
         my ($class) = @_;
         my $obj ={};
         $obj->{writer} = $writer;
+        $obj->{reader} = $reader2;
         $obj->{pid} = $pid;
         bless $obj, $class;
         return $obj;
     }
     else {
         close $writer;
-        mpv($reader);
+        close $reader2;
+        mpv($reader,$writer2);
     }
 }
 
@@ -64,6 +72,18 @@ sub set_property_string {
     my $line = "set_property_string###$args\n";
     my $writer = $obj->{writer};
     print $writer $line;
+}
+
+sub get_property_string {
+    my ($obj,@args) = @_;
+    my $args = join('###',@args);
+    my $line = "get_property_string###$args\n";
+    my $writer = $obj->{writer};
+    print $writer $line;
+    my $reader = $obj->{reader};
+    my $ret = <$reader>;
+    chomp $ret;
+    return $ret;
 }
 
 sub command {
@@ -91,13 +111,16 @@ sub terminate_destroy {
 }
 
 sub mpv {
-    my ($reader) = @_;
+    my ($reader,$writer2) = @_;
     my $initialized = 0;
     my $ctx = MPV::Simple->new();
     while (1) {
-       
-        if ( defined(my $line = <$reader>) ) {
-            chomp $line;
+        my $line =''; 
+        $line = sysreadline($reader,0);
+        chomp $line;
+        if ( $line ne "" ) {
+            #my $line = <$reader>;
+            
             my ($command, @args) = split('###',$line);
             #print "ARGS @args\n";
             #print "Executing \$ctx->$command(@args)\n";
@@ -107,9 +130,18 @@ sub mpv {
                 $initialized=0;
                 last;
             }
+            elsif ($command eq "get_property_string") {
+                my $return = $ctx->get_property_string(@args);
+                #use Devel::Peek;
+                #Dump $return;
+                # Don't forget \n at the end!!!
+                print $writer2 "$return\n";
+            }
             else {
                 eval{
-                    $ctx->$command(@args)
+                    #print "COMMAND $command\n";
+                    #print "ARGS @args\n";
+                    $ctx->$command(@args);
                 };
                 if ($@) {
                         print "FEHLER:$@\n";
@@ -121,21 +153,58 @@ sub mpv {
        if ($initialized) {
             while (my $event = $ctx->wait_event(0)) {
                 my $id = $event->id();
-                print "ID $MPV::Simple::event_names[$id] \n";
+                #print "ID $MPV::Simple::event_names[$id] \n";
                 last unless ($id);
             }
         }
         
             
     }
+    close $writer2;
     close $reader;
     exit 0;
 }
+
+# Reading non blocking
+# See Perl Cookbook 7.23
+sub sysreadline(*;$) {
+    my($handle, $timeout) = @_;
+    $handle = qualify_to_ref($handle, caller( ));
+    my $infinitely_patient = (@_ == 1 || $timeout < 0);
+    my $start_time = time( );
+    my $selector = IO::Select->new( );
+    $selector->add($handle);
+    my $line = "";
+SLEEP:
+    until (at_eol($line)) {
+        unless ($infinitely_patient) {
+            return $line if time( ) > ($start_time + $timeout);
+        }
+        # sleep only 1 second before checking again
+        next SLEEP unless $selector->can_read(1.0);
+INPUT_READY:
+        while ($selector->can_read(0.0)) {
+            my $was_blocking = $handle->blocking(0);
+CHAR:       while (sysread($handle, my $nextbyte, 1)) {
+                $line .= $nextbyte;
+                last CHAR if $nextbyte eq "\n";
+            }
+            $handle->blocking($was_blocking);
+            # if incomplete line, keep trying
+            next SLEEP unless at_eol($line);
+            last INPUT_READY;
+        }
+    }
+    return $line;
+}
+sub at_eol($) { $_[0] =~ /\n\z/ }
+
 
 DESTROY {
     my ($self) = @_;
     if ( my $pid=$self->{pid} ) {
             close $self->{writer};
+            close $self->{reader};
             kill(9,$pid);
     }
 }
